3eb24fd81403ddf49bf3960220b7f3c985e1af6d
Nick Burcin <nicholas.burcin@outlook.com>
2018-10-15 10:15:20 -0400

    Added collection view and started IBOutlets

---
+++ Coder-Swag/Coder-Swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  Coder-Swag
+//
+//  Created by Nicholas Burcin on 10/15/18.
+//  Copyright © 2018 Burcin Software. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImage!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
